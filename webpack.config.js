44e6f4af00d2ddf4759bbfaa3a6c096ec0f28f7f
Jesse Nguyen <nguyenjessev@gmail.com>
2023-01-31 22:23:45 -0500

    Configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   mode: 'development',
   output: {
     filename: 'main.js',
