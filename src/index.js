f9c74c43fe67bb36cad5c79b2e3bc4e07810e020
Jesse Nguyen <nguyenjessev@gmail.com>
2023-02-05 17:53:51 -0500

    Extract API functions to module

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-(async () => {
-  const apiKey = '505a95affb4abe534b67ce3708d862f1';
+import weatherFetcher from './weather_fetcher';
+
+(() => {
   const locationSearch = document.querySelector('#location-search');
-
-  const fetchCoords = async (query) => {
-    try {
-      const requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=${apiKey}`;
-      const response = await fetch(requestURL);
-      const data = await response.json();
-
-      return { latitude: data[0].lat, longitude: data[0].lon };
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
-
-  const fetchWeatherData = async (targetCoords) => {
-    try {
-      const requestURL = `https://api.openweathermap.org/data/2.5/weather?lat=${targetCoords.latitude}&lon=${targetCoords.longitude}&units=imperial&appid=${apiKey}`;
-      const response = await fetch(requestURL);
-
-      return await response.json();
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
 
   document
     .querySelector('.search-controls')
@@ -32,8 +10,8 @@
 
       const query = locationSearch.value || 'Fort Wayne';
 
-      const locationCoords = await fetchCoords(query);
-      const weatherData = await fetchWeatherData(locationCoords);
+      const locationCoords = await weatherFetcher.fetchCoords(query);
+      const weatherData = await weatherFetcher.fetchWeatherData(locationCoords);
 
       console.log(weatherData);
     });
