e82fdfe995f2b3d511779a41cab1eeb58580c120
Jesse Nguyen <nguyenjessev@gmail.com>
2023-02-05 19:09:46 -0500

    Extract display updating to function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import weatherFetcher from './weather_fetcher';
 
-(() => {
+(async () => {
   const locationSearch = document.querySelector('#location-search');
+  const loadWeatherData = async () => {
+    const query = locationSearch.value || 'Fort Wayne';
+    const locationCoords = await weatherFetcher.fetchCoords(query);
+    const weatherData = await weatherFetcher.fetchWeatherData(locationCoords);
+
+    return weatherData;
+  };
+  const updateWeatherDisplay = (weatherData) => {
+    try {
+      const currentLocation = weatherData.name;
+      const currentTemp = weatherData.main.temp;
+      const currentFeelsLikeTemp = weatherData.main.feels_like;
+
+      document.querySelector('.current-location').textContent = currentLocation;
+      document.querySelector('.current-temperature').textContent =
+        Math.round(currentTemp);
+
+      return true;
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  let currentWeather = await loadWeatherData();
+
+  updateWeatherDisplay(currentWeather);
 
   document
     .querySelector('.search-controls')
     .addEventListener('submit', async (event) => {
       event.preventDefault();
-
-      const query = locationSearch.value || 'Fort Wayne';
-
-      const locationCoords = await weatherFetcher.fetchCoords(query);
-      const weatherData = await weatherFetcher.fetchWeatherData(locationCoords);
-
-      console.log(weatherData);
+      currentWeather = loadWeatherData();
+      updateWeatherDisplay(currentWeather);
     });
 })();
